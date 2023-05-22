e25cf3163c68fc264f2ba2341572a379f2e37bcc
Justin Panagakis <justin.panagakis@panstack.io>
2023-05-21 21:13:33 -0400

    cleaning old stuff, srcbak

--- srcbak/ObjectClasses/PhysicalGameObject.js
+++
@@ -1,9 +0,0 @@
-class PhysicalGameObject extends GameObject { 
-    constructor(x, y, w, h) {
-        super(); 
-        this.x = x; 
-        this.y = y; 
-        this.w = w; 
-        this.h = h; 
-    }
-}
