8866b651a4674e001d15df4ee549d3df4a9f41df
justinpanagakis-panstack <justin.panagakis@panstack.io>
2023-07-02 20:23:21 -0400

    getting back into it, syncing up

--- src/ReactApp/Components/Dojo.js
+++ src/ReactApp/Components/Dojo.js
@@ -5,36 +5,14 @@ import Game from "../../GameEngine/System/Game";
 export default function Dojo() { 
     const gameContext = useContext(GameContext)
     const canvasRef = useRef(null);
-    const [ctx, setCtx] = useState(null);
-    const [game, setGame] = useState(null);
 
     useEffect(() => {
         if (canvasRef.current) {
-            setCtx(() => {
-                const newCtx = canvasRef.current.getContext('2d');
-                setGame(new Game(newCtx, gameContext))
-            })
+            const ctx = canvasRef.current.getContext('2d');
+            gameContext.newGame(ctx);
         }
 
     }, [canvasRef.current]); 
-
-    useEffect(() => {
-        if (canvasRef.current && game) {
-            game.init();
-            game.controls.setupInGameControls()
-            game.gameLoop(); 
-
-            return () => {
-                game.controls.removeInGameControls();
-            }
-        }
-    }, [game]);
-
-    useEffect(() => {
-        if (canvasRef.current && game) {
-            game.updateReactBridge(gameContext);
-        }
-    }, [gameContext]);
 
     return <canvas ref={canvasRef} width={1500} height={800} />
 }
--- src/ReactApp/GameContext.jsx
+++ src/ReactApp/GameContext.jsx
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Game from "../GameEngine/System/Game";
 
 export const GameContext = React.createContext({});
 
 export default function GameContextProvider({ children }){
 
+    const providerValueRef = useRef({})
+
+
+    const [currentGame, setCurrentGame] = useState(null);
+    const newGame = (ctx) => {
+
+        const game = new Game(ctx, providerValueRef.current);
+        game.init();
+        game.controls.setupInGameControls();
+        game.gameLoop();
+
+        setCurrentGame(game)
+    }
+
+    providerValueRef.current.newGame = newGame;
+    providerValueRef.current.currentGame = currentGame;
+
     return (
-        <GameContext.Provider value={{
-        
-        }}>{children}</GameContext.Provider>
+        <GameContext.Provider value={providerValueRef.current}>
+            {children}
+        </GameContext.Provider>
     );
 }
